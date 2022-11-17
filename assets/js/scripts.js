e3f117d4665bc6ed92771c2807a9e76f60c5842f
rpf13 <rpf13@me.com>
2022-11-17 22:53:37 +0000

    feat: Add js part for howto modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,45 @@
 /*jshint strict: global */
 /*jshint esversion: 8 */
 
+// Reference definitions to html DOM elements
+
+const howToPlayBtn = document.getElementById('howToPlayBtn');
+const howTomodal = document.getElementById('howToModal');
+const closeBtn = document.getElementsByClassName('modal-close-btn')[0];
+
+// Event Listeners
+
+howToPlayBtn.addEventListener('click', displayModal);
+closeBtn.addEventListener('click', hideModal);
+
+
+// Functions
+
+/**
+ * This function will diplay the modal window 
+ * once the related button is clicked
+ */
+
+function displayModal() {  
+    howTomodal.style.display = "block";
+}
+
+/**
+ * This function will hide the modal
+ * once the hide button is clicked
+ */
+function hideModal() {
+  howTomodal.style.display = "none";
+}
+
+/**
+ * This function will hide the modal
+ * once any area outside the modal
+ * is clicked
+ */
+
+window.onclick = function(event) {
+    if (event.target == howTomodal) {
+    howTomodal.style.display = "none";
+    }
+}
