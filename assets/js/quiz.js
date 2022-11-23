92ab7726e45ca3a333ea880178370e60d8f127d5
rpf13 <rpf13@me.com>
2022-11-23 19:17:14 +0000

    docs: Add description for each quiz js function
    This commit will add a detailed description for each function used
    in the js file. It should help to explain each of them and therefore
    help the overall understanding on how the quiz works.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -15,12 +15,22 @@ let randQuestions, currQuestIndex
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
     currQuestIndex++
+    // update here also the question count
     getNextQuestion()
 })
 
 
 // Functions section
 
+/**
+ * This function will start the game, once the Start Game button is clicked.
+ * It will hide the Start Game button and immediately display the first,
+ * randomly selected question. We use negative or positive numbers as the sort
+ * algorithm (positive one way, negative the other way). To get this neg or pos
+ * number, we use Math.rand which gives a rand number betw. 1 and 0. To become
+ * negative, we subtract 0.5. 
+ * 
+ */
 function startGame() {
     startButton.classList.add('hide')
     randQuestions = questions.sort(() => Math.random() - .5)
@@ -30,12 +40,31 @@ function startGame() {
 
 }
 
+/**
+ * This function will call the clearQuizContent function, to clear the quiz
+ * section. It will also call the displQuestion function and handover the
+ * randQuestion w. the current Question Index, as argument.
+ */
+
 function getNextQuestion() {
     clearQuizContent()
     displQuest(randQuestions[currQuestIndex])
-    // need to remove this console
+    // remove consol statement
     console.log(currQuestIndex)
 }
+
+/**
+ * This function will display the question - access the DOM element.
+ * It will take randQuestOfArray as input parameter and out of that,
+ * display k,v question.
+ * It will loop over the anser k, v pair and create a button element
+ * for each of them and set the text. If the value of the answer is correct,
+ * the dataset of that button will be set to correct. An even listener
+ * will call the chooseAnswer function and handover the event of "clicking" the button.
+ * The answerButtonElement will receive the answers as child and show them.
+ * 
+ * @param {*} randQuestOfArray result of randQuestions definition
+ */
 
 function displQuest(randQuestOfArray) {
     questElement.innerText = randQuestOfArray.question
@@ -51,6 +80,14 @@ function displQuest(randQuestOfArray) {
     });
 }
 
+/**
+ * This function will clear the whole quiz content (question, answers), once a new question
+ * gets displayed.
+ * The while loop makes sure, the buttons with the old answer are deleted. Since the new buttons
+ * with the new answers will appended in the DisplQuest function, they are 2nd child element.
+ * Therefore we have to make sure, we delete the first child element, with the old answers.
+ */
+
 function clearQuizContent() {
     nextButton.classList.add('hide')
     while (answButtonsElement.firstChild) {
@@ -58,14 +95,41 @@ function clearQuizContent() {
     }
 }
 
+/**
+ * This function will identify which button is clicked and evaluate the dataset
+ * whether it is correct or not in order to set the correct color to it. The input parameter
+ * is the click event, which is handovered from displQuest function.
+ * If the dataset of the button has the correct value, the const correct will be filled.
+ * A loop over the other, non clicked elements, which we got via answButtonElement, evaluates
+ * their dataset if correct or wrong and sets the status.
+ * @param {*} klickEvent which is defined w. event listener in displQuest function
+ */
+
 function chooseAnswer(klickEvent) {
     const klickedButton = klickEvent.target
     const correct = klickedButton.dataset.correct
     Array.from(answButtonsElement.children).forEach(button => {
         setState(button, button.dataset.correct)
     })
-    nextButton.classList.remove('hide')
+    if (randQuestions.length > currQuestIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        // remove or update this function to if we are at the end of q's we end game and message
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
 }
+
+/**
+ * This function will take the actual button element and the status of the dataset, whether it
+ * is correct or not.
+ * The status of the element will be cleared to make sure, we can set the proper state. The
+ * clearState function is called for this and the element is handovered.
+ * If the state of the button dataset element is correct, we set the class correct, if it is
+ * incorrect, we set the class wrong.
+ * @param {*} element the actual button element
+ * @param {*} correct status of the dataset whether correct or wrong
+ */
 
 function setState(element, correct) {
     clearState(element)
@@ -76,6 +140,11 @@ function setState(element, correct) {
         element.classList.add('wrong')
     }
 }
+
+/**
+ * This function will take the button class element (correct or wrong) we got from setState function
+ * @param {*} clearElement The button class element altered from setState function
+ */
 
 function clearState(clearElement) {
     clearElement.classList.remove('correct')
