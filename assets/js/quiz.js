7b6d7ce884b53760fa3b513d778176fb4fd97873
rpf13 <rpf13@me.com>
2022-11-23 14:29:54 +0000

    feat: Add & display random question
    This commit will enhance the function to pick a random question out
    of the question pool. It will also set the index for the current
    question, which is needed to check the total amount, when creating
    the counter later on.
    It will then write the question back to the DOM

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,7 +2,12 @@
 
 const startButton = document.getElementById('start-btn')
 const questContElement = document.getElementById('quest-cont')
+const questElement = document.getElementById('quiz-question')
+const answButtonsElement = document.getElementById('quiz-answers')
 
+// define Variables, which are going to be changed in the function
+
+let randQuestions, currQuestIndex
 
 // Event listeners section
 
@@ -13,13 +18,21 @@ startButton.addEventListener('click', startGame)
 
 function startGame() {
     startButton.classList.add('hide')
+    randQuestions = questions.sort(() => Math.random() - .5)
+    currQuestIndex = 0
     questContElement.classList.remove('hide')
     getNextQuestion()
 
 }
 
 function getNextQuestion() {
+    displQuest(randQuestions[currQuestIndex])
+    // need to remove this console
+    console.log(currQuestIndex)
+}
 
+function displQuest(randQuestOfArray) {
+    questElement.innerText = randQuestOfArray.question
 }
 
 function chooseAnswer() {
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,3 @@
-"use strict";
-
-/*jshint strict: global */
-/*jshint esversion: 8 */
 
 // Reference definitions to html DOM elements
 
