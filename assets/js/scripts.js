cb51172711863cc9df6a74d785f1e13a18c97ec6
rpf13 <rpf13@me.com>
2022-11-27 19:28:13 +0000

    fix: Correct feedback form call
    This commit will fix the feedback form call via js function. After
    the migration of the quiz section to the index, the function did
    not properly work. This commit fixes this problem.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,38 +1,41 @@
 
 // Reference definitions to html DOM elements
 
-const howToPlayBtn = document.getElementById('howToPlayBtn');
-const howToModal = document.getElementById('howToModal');
-const closeBtn = document.getElementsByClassName('modal-close-btn')[0];
-const feedbackBtn = document.getElementById('feedbackBtn');
+// const howToPlayBtn = document.getElementById('howToPlayBtn');
+// const howToModal = document.getElementById('howToModal');
+// const closeBtn = document.getElementsByClassName('modal-close-btn')[0];
+// const feedbackBtn = document.getElementById('feedbackBtn');
+
+
+// startBtn no longer needed
 const startBtn = document.getElementById('startBtn');
 
 // Event Listeners
 
-howToPlayBtn.addEventListener('click', displayModal);
-closeBtn.addEventListener('click', hideModal);
-feedbackBtn.addEventListener('click', goToForm);
+// howToPlayBtn.addEventListener('click', displayModal);
+// closeBtn.addEventListener('click', hideModal);
+// feedbackBtn.addEventListener('click', goToForm);
 startBtn.addEventListener('click', goToQuiz);
 
 
 // Functions
 
-/**
- * This function will diplay the modal window 
- * once the related button is clicked
- */
+// /**
+//  * This function will diplay the modal window 
+//  * once the related button is clicked
+//  */
 
-function displayModal() {  
-    howToModal.style.display = "block";
-}
+// function displayModal() {  
+//     howToModal.style.display = "block";
+// }
 
 /**
  * This function will hide the modal
  * once the hide button is clicked
  */
-function hideModal() {
-  howToModal.style.display = "none";
-}
+// function hideModal() {
+//   howToModal.style.display = "none";
+// }
 
 /**
  * This function will hide the modal
@@ -40,18 +43,18 @@ function hideModal() {
  * is clicked
  */
 
-window.onclick = function(event) {
-    if (event.target == howToModal) {
-    howToModal.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target == howToModal) {
+//     howToModal.style.display = "none";
+//     }
+// }
 
 /**
  * This function will move the current view to the contact form section
  */
-function goToForm() {
-  location.href = "#form";
-}
+// function goToForm() {
+//   location.href = "#form";
+// }
 
 /**
  * This function will move the current view to the contact form section
